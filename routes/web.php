b023f06244a69480c13176f7fffcaf1762e1e964
EoBrad <joao.dev321@gmail.com>
2023-05-12 11:34:13 -0300

    HEY-11: It should list all the questions

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\{ProfileController, QuestionController};
+use App\Http\Controllers\{DashboardController, ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if(app()->isLocal()) {
+    if (app()->isLocal()) {
 
         auth()->loginUsingId(1);
 
@@ -14,9 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::post('/question/store', [QuestionController::class, 'store'])->name('question.store');
 
