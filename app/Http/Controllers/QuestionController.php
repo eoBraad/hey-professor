eb5e8d273c11f344b3532941bcd815deeebb0041
João Pontes <joao.alencar099@gmail.com>
2023-09-03 11:17:45 -0300

    hey-14: it should make sure that only the person who has created can destroy the question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Question;
 use Closure;
 use Illuminate\Http\{RedirectResponse, Request};
 use Illuminate\View\View;
@@ -34,6 +35,15 @@ class QuestionController extends Controller
                 'question' => $request->question,
                 'draft'    => true,
             ]);
+
+        return back();
+    }
+
+    public function destroy(Question $question): RedirectResponse
+    {
+        $this->authorize('destroy', $question);
+
+        $question->delete();
 
         return back();
     }
