9b515c15dadd99b0710848027c79654bd50f04f1
João Pontes <joao.alencar099@gmail.com>
2023-09-04 13:53:33 -0300

    hey-16: it should make sure that only question with status DRAFT and only if the question was created by logged user can be update

--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -19,6 +19,41 @@ it('should be able to update a question', function () {
     expect($question->question)->toBe('updated the question?');
 });
 
-it('should update the question in the database', function () {
+it('should make sure that only question with status DRAFT can be update', function () {
+    $user = User::factory()->create();
 
-})->todo();
+    $questionNotDraft = Question::factory()->for($user, 'createdBy')->create(['draft' => false]);
+
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+
+    actingAs($user);
+
+    put(route('question.update', $questionNotDraft), [
+        'question' => 'updated a question?',
+    ])->assertForbidden();
+
+    put(route('question.update', $question), [
+        'question' => 'updated a question?',
+    ])->assertRedirect();
+
+    $question->refresh();
+    expect($question->question)->toBe('updated a question?');
+});
+
+it("should make sure that only the person who has created can update the question", function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+    $question  = Question::factory()->create(["draft" => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+
+    put(route("question.update", $question), [
+        'question' => 'updated a question?',
+    ])->assertForbidden();
+
+    actingAs($rightUser);
+
+    put(route("question.update", $question), [
+        'question' => 'updated a question?',
+    ])->assertRedirect();
+});
