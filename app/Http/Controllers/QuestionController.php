aa138efe88dda1ae5d090cf072bf21e3319f6f6e
João Pontes <joao.alencar099@gmail.com>
2023-09-04 16:04:39 -0300

    hey-16: it should be possible to validate if the question has min 10 characters and if it ends with question mark ?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -55,9 +55,21 @@ class QuestionController extends Controller
         return view('question.edit', ['question' => $question]);
     }
 
-    public function update(Question $question): RedirectResponse
+    public function update(Question $question, Request $request): RedirectResponse
     {
-        $this->authorize('update', $question);
+        $this->authorize('update', $question, );
+
+        $request->validate([
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    if($value[strlen($value) - 1] != '?') {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end.');
+                    }
+                },
+            ],
+        ]);
 
         $question->question = \request()->question;
         $question->save();
--- tests/Feature/Question/UpdateTest.php
+++ tests/Feature/Question/UpdateTest.php
@@ -2,7 +2,7 @@
 
 use App\Models\{Question, User};
 
-use function Pest\Laravel\{actingAs, put};
+use function Pest\Laravel\{actingAs, assertDatabaseCount, assertDatabaseHas, put};
 
 it('should be able to update a question', function () {
     $user     = User::factory()->create();
@@ -56,4 +56,63 @@ it("should make sure that only the person who has created can update the questio
     put(route("question.update", $question), [
         'question' => 'updated a question?',
     ])->assertRedirect();
+});
+
+it('should be able update a new question bigger than 255 characters', function () {
+    // Arrange :: Preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    // Act :: Agir
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 256) . '?',
+    ]);
+
+    // Assert :: Verificar
+    $request->assertRedirect();
+    assertDatabaseHas('questions', ['question' => str_repeat('*', 256) . '?']);
+
+});
+
+it('should check if ends with question mark ?', function () {
+
+    // Arrange :: Preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    // Act :: Agir
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 10),
+    ]);
+
+    // Assert :: Verificar
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end.',
+    ]);
+
+    $question->refresh();
+
+    expect($question->question)->not->toBe(str_repeat('*', 10));
+});
+
+it('should have at least 10 characters', function () {
+
+    // Arrange :: Preparar
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $question = Question::factory()->for($user, 'createdBy')->create(['draft' => true]);
+    // Act :: Agir
+    $request = put(route('question.update', $question), [
+        'question' => str_repeat('*', 8) . '?',
+    ]);
+
+    // Assert :: Verificar
+    $request->assertSessionHasErrors(['question' => __('validation.min.string', ['min' => 10, 'attribute' => 'question'])]);
+
+    $question->refresh();
+
+    expect(str_word_count($question->question))->not->toBe(8);
 });
