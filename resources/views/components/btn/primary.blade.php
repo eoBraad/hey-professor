f85dae1a50d57e42c742c02eb5a73672e9c27238
João Pontes <joao.alencar099@gmail.com>
2023-09-07 19:28:13 -0300

    hey-24: applying filter in the frontend

--- resources/views/components/btn/primary.blade.php
+++ resources/views/components/btn/primary.blade.php
@@ -2,6 +2,6 @@
 
 
 <button type={{ $type }}
-    class="mr-2 mb-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+    class="mr-2 rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     {{ $slot }}
 </button>
